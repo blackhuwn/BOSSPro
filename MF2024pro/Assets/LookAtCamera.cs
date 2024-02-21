3e1d08f538af3d85bed40a8a9f2b0bd2defaf5e3
blackhuwn <blackhuwn_BOSS@163.com>
2024-02-21 16:48:34 +0800

    修改

--- MF2024pro/Assets/LookAtCamera.cs
+++ MF2024pro/Assets/LookAtCamera.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour
 {
     private Transform mainCameraTransform;
+    public GameObject Child;
 
     void Start()
     {
         mainCameraTransform = GameObject.FindObjectOfType<Camera>().transform;
+        float xV = -Child.transform.localScale.x;
+        Child.transform.localScale = new Vector3(xV, Child.transform.localScale.y, Child.transform.localScale.z);
+        //    Child.transform.rotation
     }
 
     void Update()
     {
         transform.LookAt(mainCameraTransform);
+
     }
 }
+
+    
+
