4d01a5e8e0e2ffc9a515e236039675d6ffcbeb32
blackhuwn <blackhuwn_BOSS@163.com>
2024-02-21 17:01:36 +0800

    上传物理属性

--- MF2024pro/Assets/PlayerController.cs
+++ MF2024pro/Assets/PlayerController.cs
@@ -6,9 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float rotateSpeed = 100f;
+    public float jumpForce = 30f;
+    private Rigidbody rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
